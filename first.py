4f37a43627b548dd2f1f50d547071e6cacf8f048
dhanvantrikulkarni <153621822+dhanvantrikulkarni@users.noreply.github.com>
2024-12-19 15:48:15 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,7 @@
+a="gadag"
+b=a[::-1]
+if a==b:
+    print('pallindrome')
+else:
+    print("not a pallindrome")
+    C:\Users\danva\OneDrive\Documents\Heart Disease Project\Heart Disease Project\templates\first.py
